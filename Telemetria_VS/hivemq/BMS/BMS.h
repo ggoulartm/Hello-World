fed4e1c8d0536016e68ae8d761a07bdd002eab13
ggoulartm <ggoulartm@gmail.com>
2022-11-28 18:36:57 -0300

    HiveMQ

---
+++ Telemetria_VS/hivemq/BMS/BMS.h
@@ -0,0 +1,38 @@
+#ifndef BATTERYMANAGEMENTSYSTEM
+#define BATTERYMANAGEMENTSYSTEM
+#include <mcp_can.h>
+#endif
+#define SENDCONFIG 0
+#define REQUEST_CONFIG 0
+#define CAN0_INT 2                              // Set INT to pin 2
+
+class BMS{
+int NCH=8;                // [numero de celulas]
+int NTH=4;                // [numero de termistores]   
+int OV=3650;              // [Sobretensão - mV]
+int UV=2850;              // [Subtensão - mV]
+int SHORTCIRCUIT=50000;   //[corrente de curto - mA]
+int OVERCURRENT=40000;    //[sobrecorrente - mA]
+int OVERTEMPERATURE=45;   //[sobretemperatura - ºC]
+int GAIN_I_SENSE=2083;    //[sensibilidade do sensor de corrente - uV/A]
+int DVOLT=8;              //[delta de tensão de balanceamento - mV]
+int LOWVOLT=3000;         //[tensão de alerta de baixa tensão mV]
+int HIGHVOLT=3500;        //[tensão de alerta de alta tensão mV]
+int CANCHARGEVOLT=3200;   //[não utilizado] 
+int CHARGEDVOLT=3600;       //[tensão que define que bateria está carregada mV]
+int OVERVOLT_HISTERESYS=4;  //[NÃO UTILIZADO]
+int DELAY_HISTERESYS=60;    //[tempo maximo de balanceamento - minutos]
+int BALANCE=1;              //[define se é permitido balanceamento - binario]
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+int readcan=0;
+int Vpack,Current1,Current2,SoC;
+    public:
+        BMS();
+        void can_intialize();
+        //void sendConfig();
+        void requestConfig();
+        int *readCan();
+};
